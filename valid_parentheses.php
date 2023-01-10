2be60958186874d2adadb6667a58d439aa023840
shyqulreyad <shyqulislam@gmail.com>
2023-01-10 14:51:12 +0600

    valid parentheses 2nd attempt

--- valid_parentheses.php
+++ valid_parentheses.php
@@ -1,41 +1,30 @@
 <?php
-$s = "{[]}";
-// sort($s);
+$s = "({[]})";
 // $s = "([)]";
 $s = str_split($s);
-$len = count($s);
+$len = count($s);  //4
 $result = true;
-for($i=0;$i < $len; $i+=2){
-    if($s[$i] == '(' && $s[$i+1] != ')'){
-        if($s[$i+2] !== ')'){
-            $result = false;
-        }
-        if($s[$i+3] != ')'){
-            $result = false;
-        }
-        $result = false;
-    }elseif($s[$i] == ')'){
-        $result = false;
-    }elseif($s[$i] == '{' && $s[$i+1] != '}'){
-        if($s[$i+2] !== '}'){
-            $result = false;
-        }
-        if($s[$i+3] != '}'){
-            $result = false;
-        }
-        $result = false;
-    }elseif($s[$i] == '}'){
-        $result = false;
-    }elseif($s[$i] == '[' && $s[$i+1] != ']'){
-        if($s[$i+2] !== ']'){
-            $result = false;
-        }
-        if($s[$i+3] != ']'){
-            $result = false;
-        }
-        $result = false;
-    }elseif($s[$i] == ']'){
-        $result = false;
-    }
+if($s[0] ==')' || $s[0] == '}' || $s[0] == ']'){
+    $result = false;
+    return $result;
 }
-print_r($result);
+if($s[$len - 1] == '(' || $s[$len -1] == '[' || $s[$len -1] == '{'){
+    $result = false;
+    return $result;
+}
+if($len % 2 == 0){
+    $half_len = $len / 2;
+        for($i=0;$i < $half_len; $i++){
+            for($j = $len-1; $j > $half_len ; $j--){
+                print('first'.$s[$i]);
+                print('second'.$s[$j]);
+            }
+         
+        }
+}else{
+    $result = false;
+    return $result;
+}
+
+// print_r($result);
+
