484a9bafcd718ba61b4c4cc8cad15841b27c6aac
shyqulreyad <shyqulislam@gmail.com>
2023-01-18 08:54:34 +0600

    18th jan

--- House Robber.py
+++ House Robber.py
@@ -1,17 +1,38 @@
+# nums = [1,2,3,1]
+# nums= [2,7,9,3,1]
+# nums = [2,1,1,2]
+# nums = [1,2,3,4,5,6]
 nums = [1,2,3,1]
-nums= [2,7,9,3,1]
-nums = [2,1,1,2]
 length = len(nums)
-result_even = 0
-result_odd = 0
+previ= 0
+prevj=0
+even_max = 0
+odd_max = 0
 i=0
+# j = length-1
 while i < length:
-    if i%2 == 0:
-        result_even +=nums[i]
-    else:
-        result_odd += nums[i]
-    i+=1
-if result_even > result_odd:
-    print(result_even)
+    if i+1 < length:
+        j = i+1
+        # print(j-i)
+        print(previ,'previ',prevj,'prevj')
+        print(nums[i],'i')
+        if odd_max + prevj > odd_max + nums[i] and i-(j-1) != 1:
+            odd_max += prevj
+        else:
+            odd_max +=nums[i]
+        # print(odd_max,'i max')
+        previ = nums[i]
+        print(nums[j],'j')
+        if even_max +previ > even_max + nums[j] and j-(i-1) != 1:
+            even_max +=previ
+        else:
+            even_max += nums[j]
+        # print(even_max,'j max')
+        prevj = nums[j]
+
+    i+=2
+# print(even_max,odd_max)
+if even_max > odd_max:
+    print(even_max)
 else:
-    print(result_odd)
+    print(odd_max)
--- solved/best_time_to_sell_2.py
+++ solved/best_time_to_sell_2.py
@@ -1,6 +1,7 @@
-prices = [7,1,5,3,6,4]
+# prices = [7,1,5,3,6,4]
 # prices = [7,6,4,3,1]
 # prices = [1,2,3,4,5]
+prices =[3,3,5,0,0,3,1,4]
 
 length = len(prices)
 min_price = max(prices)
@@ -11,9 +12,16 @@ while i < length:
     if prices[i] < min_price:
         min_price = prices[i]
     profit = prices[i]- min_price
+    # print(prices[i],'---',min_price)
+    # print(profit)
     if profit > max_profit:
+        print(profit)
         max_profit = profit
-        print(max_profit)
+        total +=profit
+        min_price = prices[i]
+        max_profit = 0
+        i-=1
+        # print(max_profit)
         # min_price = max(prices)
     i+=1
-print(max_profit)
+print(total)
