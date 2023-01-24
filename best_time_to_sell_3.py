598d7674990052fd7a2a0aad634c1499f3629617
shyqulreyad <shyqulislam@gmail.com>
2023-01-24 15:16:01 +0600

    24th jan

--- best_time_to_sell_3.py
+++ best_time_to_sell_3.py
@@ -1,29 +1,29 @@
 # prices = [7,1,5,3,6,4]
 # prices = [7,6,4,3,1]
-prices = [1,2,3,4,5]
+# prices = [1,2,3,4,5]
 # prices =[3,3,5,0,0,3,1,4]
-length = len(prices)
-min_price = max(prices)
-max_profit = 0
-total =[]
-i =0
-while i < length:
-    if prices[i] < min_price:
-        min_price = prices[i]
-    profit = prices[i]- min_price
-    print(prices[i],'---',min_price)
-    print(profit)
-    if profit > max_profit:
-        # print(profit)
-        max_profit = profit
-        total.append(profit)
-        min_price = prices[i]
-        max_profit = 0
-        i-=1
-        print(max_profit)
-        min_price = max(prices)
-    i+=1
-print(total)
+# length = len(prices)
+# min_price = max(prices)
+# max_profit = 0
+# total =[]
+# i =0
+# while i < length:
+#     if prices[i] < min_price:
+#         min_price = prices[i]
+#     profit = prices[i]- min_price
+#     print(prices[i],'---',min_price)
+#     print(profit)
+#     if profit > max_profit:
+#         # print(profit)
+#         max_profit = profit
+#         total.append(profit)
+#         min_price = prices[i]
+#         max_profit = 0
+#         i-=1
+#         print(max_profit)
+#         min_price = max(prices)
+#     i+=1
+# print(total)
 # lenght = len(total)
 # if lenght <=2:
 #     print(sum(total),'hi')
@@ -36,3 +36,53 @@ print(total)
 #             res = prof
 #     i+=1
 # print(res,'res')
+
+
+# prices = [1,2,3,4,5,1,10]
+# prices = [7,6,4,3,1]
+# prices = [3,3,5,0,0,3,1,4]
+# # prices = [1,2,3,4,5]
+# length = len(prices)
+# min_price = max(prices)
+# max_profit = 0
+# max_array = []
+# i =0
+# while i < length:
+#     if prices[i] < min_price:
+#         min_price = prices[i]
+#     profit = prices[i]- min_price
+#     print(prices[i],'---',min_price,'===',profit)
+#     if profit > max_profit:
+#         print(profit)
+#         # min_price = prices[i]
+#         max_profit = profit
+#         # max_array.append(max_profit)
+#     i+=1
+# print(max_array)
+prices = [7,6,4,3,1]
+prices = [1,2,3,4,5]
+# prices =[3,3,5,0,0,3,1,4]
+# prices =[3,3,5,0,0,3,1,4]
+
+length = len(prices)
+min_price = max(prices)
+max_profit = 0
+total = 0
+i =0
+while i < length:
+    if prices[i] < min_price:
+        min_price = prices[i]
+    profit = prices[i]- min_price
+    # print(prices[i],'---',min_price)
+    # print(profit)
+    if profit > max_profit:
+        print(profit)
+        max_profit = profit
+        total +=profit
+        min_price = prices[i]
+        max_profit = 0
+        i-=1
+        # print(max_profit)
+        # min_price = max(prices)
+    i+=1
+# print(total)
--- solved/best_time_to_sell_2.py
+++ solved/best_time_to_sell_2.py
@@ -1,7 +1,9 @@
 # prices = [7,1,5,3,6,4]
 # prices = [7,6,4,3,1]
 # prices = [1,2,3,4,5]
-prices =[3,3,5,0,0,3,1,4]
+# prices =[3,3,5,0,0,3,1,4]
+prices = [1,2,3,0,2]
+prices =[1,2,4]
 
 length = len(prices)
 min_price = max(prices)
@@ -15,12 +17,11 @@ while i < length:
     # print(prices[i],'---',min_price)
     # print(profit)
     if profit > max_profit:
-        print(profit)
         max_profit = profit
         total +=profit
         min_price = prices[i]
         max_profit = 0
-        i-=1
+        i+=1
         # print(max_profit)
         # min_price = max(prices)
     i+=1
