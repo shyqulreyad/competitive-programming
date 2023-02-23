7dc5cc5381d6fea0ffab31bf66a50779601ce8ff
shyqulreyad <shyqulislam@gmail.com>
2023-02-23 23:04:54 +0600

    how sum

--- DSA/DP/HowSum.py
+++ DSA/DP/HowSum.py
@@ -1,14 +1,34 @@
 #how sum with out dp
-def howsum(target,nums):
+# def howsum(target,nums):
+#     if target == 0:
+#         return []
+#     if target<0:
+#         return None
+#     for i in nums:
+#         remainder = target -i
+#         res = howsum(remainder,nums)
+#         if res != None:
+#             return [*res,i]
+#     return None
+
+# print(howsum(7,[2,4]))
+#how sum with dp
+def howsumdp(target,nums,memo={}):
+    if target in memo:
+        return memo[target]
     if target == 0:
         return []
     if target<0:
         return None
     for i in nums:
         remainder = target -i
-        res = howsum(remainder,nums)
+        res = howsumdp(remainder,nums,memo)
         if res != None:
+            memo[target] = [*res,i]
+            # print(memo)
             return [*res,i]
+    memo[target] = None
     return None
 
-print(howsum(7,[2,4]))
+# print(howsumdp(300,[7,14]))
+print(howsumdp(7,[2,3,6,7]))
