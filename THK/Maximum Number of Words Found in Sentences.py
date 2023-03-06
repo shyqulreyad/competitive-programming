5b68db8fcbaab6d85dca1b68600495465a600c13
shyqulreyad <shyqulislam@gmail.com>
2023-03-06 10:28:28 +0600

    6th march

--- THK/Maximum Number of Words Found in Sentences.py
+++
@@ -1,8 +0,0 @@
-sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
-count = 0
-for i in sentences:
-    j =i.split(' ')
-    count = len(j)
-    if len(j) > count:
-        count = len(j)
-print(count)
---
+++ solved/Maximum Number of Words Found in Sentences.py
@@ -0,0 +1,8 @@
+sentences =["w jrpihe zsyqn l dxchifbxlasaehj","nmmfrwyl jscqyxk a xfibiooix xolyqfdspkliyejsnksfewbjom","xnleojowaxwpyogyrayfgyuzhgtdzrsyococuqexggigtberizdzlyrdsfvryiynhg","krpwiazoulcixkkeyogizvicdkbrsiiuhizhkxdpssynfzuigvcbovm","rgmz rgztiup wqnvbucfqcyjivvoeedyxvjsmtqwpqpxmzdupfyfeewxegrlbjtsjkusyektigr","o lgsbechr lqcgfiat pkqdutzrq iveyv iqzgvyddyoqqmqerbmkxlbtmdtkinlk","hrvh efqvjilibdqxjlpmanmogiossjyxepotezo","qstd zui nbbohtuk","qsdrerdzjvhxjqchvuewevyzlkyydpeeblpc"]
+count = 0
+for i in sentences:
+    j =i.split(' ')
+    print(count)
+    if len(j) > count:
+        count = len(j)
+print(count)
