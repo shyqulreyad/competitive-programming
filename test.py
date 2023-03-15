67596d42cb769d2932bc685612a2aa75b3bf253b
shyqulreyad <shyqulislam@gmail.com>
2023-03-15 14:00:59 +0600

    15th march

--- test.py
+++ test.py
@@ -3,18 +3,18 @@
 #     print(i)
 #     for j in digits:
 #         print(j,'test')
-# nums = [7,2,8,5,0,9,1,2,9,5,4,9,4,7,1,1,1,7,4,6]
-# print(nums)
-# lenth = len(nums)
-# print(lenth)
-# i=0
-# while i <lenth:
-#     if nums[i] == 0:
-#         print('deleted')
-#         print(lenth)
-#         del(nums[i])
-#     i+=1
-# print(nums)
+nums = [7,2,8,5,0,9,1,2,9,5,4,9,4,7,1,1,1,7,4,6]
+print(nums)
+lenth = len(nums)
+print(lenth)
+i=0
+while i <lenth:
+    if nums[i] == 0:
+        print('deleted')
+        print(lenth)
+        del(nums[i])
+    i+=1
+print(nums)
 # def bin(n):
 #     if n > 1:
 #         bin(n//2)
