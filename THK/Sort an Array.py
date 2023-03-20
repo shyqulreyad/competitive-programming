8b4c1a63154b94bb655f3bddfd0ea8c9838e99bc
shyqulreyad <shyqulislam@gmail.com>
2023-03-20 06:42:40 +0600

    20th march

--- THK/Sort an Array.py
+++ THK/Sort an Array.py
@@ -1,6 +1,27 @@
 nums = [5,2,3,1]
-nums.sort()
-print(nums) 
-for i in nums:
-    print(i)
-    
+#sort nums with merge sort algorithm
+def mergeSort(nums):
+    if len(nums) > 1:
+        mid = len(nums)//2
+        left = nums[:mid]
+        right = nums[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+mergeSort(nums)
