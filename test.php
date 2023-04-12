89ba4e06fc5b6b26239c96654e5393b965ae46da
shyqulreyad <shyqulislam@gmail.com>
2023-04-12 13:15:22 +0600

    recursion

--- test.php
+++ test.php
@@ -3,3 +3,30 @@ $num = [1,2,0,0];
 $k = 34;
 $sum = $k + implode($num);
 print_r(str_split($sum));
+// create a recursive function
+function sum($num, $k) {
+    $sum = $k + implode($num);
+    $sum = str_split($sum);
+    if (in_array(0, $sum)) {
+        $sum = array_diff($sum, [0]);
+        $sum = array_values($sum);
+        return sum($sum, 0);
+    } else {
+        return $sum;
+    }
+}
+print_r(sum($num, $k));
+
+// create recursive funtion using dp (dynamic programming)
+function sum_dp($num, $k) {
+    $sum = $k + implode($num);
+    $sum = str_split($sum);
+    if (in_array(0, $sum)) {
+        $sum = array_diff($sum, [0]);
+        $sum = array_values($sum);
+        return sum_dp($sum, 0);
+    } else {
+        return $sum;
+    }
+}
+print_r(sum_dp($num, $k));
