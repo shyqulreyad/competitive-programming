f1cc3351a2f0e63912dd0c77a3f8aeaa1dabc55c
shyqul reyad <66216627+shyqulreyad@users.noreply.github.com>
2023-04-22 21:28:35 +0600

    Update test.js
    22nd april

--- test.js
+++ test.js
@@ -15,3 +15,20 @@ return nums
 }
 rm(nums)
 
+
+
+let nums = [0,0,1,1,1,1,2,3,3]
+
+var rm=function(nums){
+    let j=0
+for(let i=0;i<nums.length;i++){
+    if(nums[i]!==nums[i+2]){
+        nums[j]=nums[i]
+        j++
+    }
+}
+return nums
+
+}
+rm(nums)
+
