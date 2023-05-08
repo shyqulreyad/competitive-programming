3c5e5b6b7a932fced2c186939c9073f5e37b66bb
shyqulreyad <shyqulislam@gmail.com>
2023-05-08 11:15:17 +0600

    8th may

--- test.py
+++ test.py
@@ -354,69 +354,69 @@
 "+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
 The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
 """
-# nums = [4,4,4,9,2,4]
-# nums = [29,47,21,41,13,37,25,7]
-# nums = [0,1,2,2,4,4,1]
-# length = len(nums)
-# prev = 0
-# counter = 1
-# i =0
-# while i <length:
-#     # print(nums[i])
-#     if nums[i] % 2 == 0:
-#         prev = nums[i]
-#         # print(nums[i])
-#         if prev == nums[i]:
-#             counter+=1
-#         else:
-#             counter =1
+nums = [4,4,4,9,2,4]
+nums = [29,47,21,41,13,37,25,7]
+nums = [0,1,2,2,4,4,1]
+length = len(nums)
+prev = 0
+counter = 1
+i =0
+while i <length:
+    # print(nums[i])
+    if nums[i] % 2 == 0:
+        prev = nums[i]
+        # print(nums[i])
+        if prev == nums[i]:
+            counter+=1
+        else:
+            counter =1
         
-#     else:
-#         counter =1
-#     i+=1
-# print(counter)
+    else:
+        counter =1
+    i+=1
+print(counter)
 
 
-# nums = [4,5,6,7,0,1,2]
-# target = 0
-# if target in nums:
-#     print(nums.index(target))
-# else:
-#     print(-1)
+nums = [4,5,6,7,0,1,2]
+target = 0
+if target in nums:
+    print(nums.index(target))
+else:
+    print(-1)
 
 
-# nums = [1,2,3,1,1,3]
-# counter = 0
-# for i in range(len(nums)):
-#     for j in range(i+1,len(nums)):
-#         if nums[i] == nums[j] and i <j:
-#             counter+=1            
-# print(counter)
+nums = [1,2,3,1,1,3]
+counter = 0
+for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+        if nums[i] == nums[j] and i <j:
+            counter+=1            
+print(counter)
 
-# n = 6
-# if n%2 ==0:
-#     print(n)
-# else:
-#     print(n*2)
+n = 6
+if n%2 ==0:
+    print(n)
+else:
+    print(n*2)
 
-# s = "is2 sentence4 This1 a3"
-# x = s.split()
-# lenght = len(x)
-# res ={}
-# for i in x :
-#     k = list(i)
-#     index =int(len(k)-1)
-#     target = int(k[index])
-#     del k[index]
-#     l = ''.join(k)
-#     res[target-1] = l
-# result = ''
-# for i in range(lenght):
-#     if i == 0:
-#         result += res[i]
-#     else:
-#         result +=' '+ res[i]
-# print(result)
+s = "is2 sentence4 This1 a3"
+x = s.split()
+lenght = len(x)
+res ={}
+for i in x :
+    k = list(i)
+    index =int(len(k)-1)
+    target = int(k[index])
+    del k[index]
+    l = ''.join(k)
+    res[target-1] = l
+result = ''
+for i in range(lenght):
+    if i == 0:
+        result += res[i]
+    else:
+        result +=' '+ res[i]
+print(result)
 
 s = "leetcode" 
 wordDict = ["leet","code"]
