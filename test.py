c36c86703980fdd35ef9856e02f5640b99c4a705
shyqulreyad <shyqulislam@gmail.com>
2023-05-19 11:54:35 +0600

    19th may

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 #String compression
 chars =["a","a","b","b","c","c","c"]
 chars_stirng = ''.join(chars)
-chars = []
-for i in chars_stirng:
-    if i not in chars:
-        chars.append(i)
-        chars.append(str(chars_stirng.count(i)))
+# chars = []
+# for i in chars_stirng:
+#     if i not in chars:
+#         chars.append(i)
+#         chars.append(str(chars_stirng.count(i)))
         
 print(chars)
 
