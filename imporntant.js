c165c13f78487dba28dc0fb60c3f6a6b60af057c
shyqulreyad <shyqulislam@gmail.com>
2023-06-12 18:18:23 +0600

    important js

---
+++ imporntant.js
@@ -0,0 +1,33 @@
+// to get all the check box value
+$('.checkboxclass:checked').each(function() {
+    if ($(this).is(':checked')) {
+        var checked = ($(this).val());
+        session_id.push(checked);
+    }
+})
+
+// to mark all check box
+$(document).on('click', '.check_all', function() {
+    if ($(this).prop('checked') == true) {
+        $('.data_checkbox').each(function() {
+            if ($(this).prop('disabled')) {
+                $(this).prop('checked', false);
+            } else {
+                $(this).prop('checked', true);
+            }
+
+        })
+    }
+
+
+    if ($(this).prop('checked') == false) {
+        $('.data_checkbox').each(function() {
+            if ($(this).prop('disabled')) {
+                $(this).prop('checked', false);
+            } else {
+                $(this).prop('checked', false);
+            }
+        })
+    }
+
+});
