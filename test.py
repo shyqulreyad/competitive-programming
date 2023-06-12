d8d75e15b995d8a5f263b033cdccb21604db7bfd
shyqulreyad <shyqulislam@gmail.com>
2023-06-12 11:54:57 +0600

    12th june

--- test.py
+++ test.py
@@ -469,58 +469,58 @@ The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
 # else:
 #     print('false')
 
-s = "Let's take LeetCode contest"
-s = "God Ding"
-res =''
-x = s.split()
-for idx, i in enumerate(x):
-    if idx == 0:
-        res+=i[::-1]
-    else:
-        res+= ' '+i[::-1]
-print(res)
+# s = "Let's take LeetCode contest"
+# s = "God Ding"
+# res =''
+# x = s.split()
+# for idx, i in enumerate(x):
+#     if idx == 0:
+#         res+=i[::-1]
+#     else:
+#         res+= ' '+i[::-1]
+# print(res)
 
 
-prices = [7,1,5,3,6,4]
-prices = [7,6,4,3,1]
-prices = [1,2,3,4,5]
-prices =[3,3,5,0,0,3,1,4]
-prices = [1,2,3,0,2]
-prices =[1,2,4]
-prices = [1,3,2,8,4,9]
-# prices = [1,3,7,5,10,3]
-length = len(prices)
-min_price = max(prices)
-max_profit = 0
-total = 0
-i =0
-while i < length:
-    if prices[i] < min_price:
-        min_price = prices[i]
-    profit = prices[i]- min_price
-    # print(prices[i],'---',min_price)
-    # print(profit)
-    if profit > max_profit and profit > 2:
-        max_profit = profit
-        total +=profit
-        min_price = prices[i]
-        max_profit = 0
-        i+=1
-        # print(max_profit)
-        # min_price = max(prices)
-    i+=1
-print(total)
+# prices = [7,1,5,3,6,4]
+# prices = [7,6,4,3,1]
+# prices = [1,2,3,4,5]
+# prices =[3,3,5,0,0,3,1,4]
+# prices = [1,2,3,0,2]
+# prices =[1,2,4]
+# prices = [1,3,2,8,4,9]
+# # prices = [1,3,7,5,10,3]
+# length = len(prices)
+# min_price = max(prices)
+# max_profit = 0
+# total = 0
+# i =0
+# while i < length:
+#     if prices[i] < min_price:
+#         min_price = prices[i]
+#     profit = prices[i]- min_price
+#     # print(prices[i],'---',min_price)
+#     # print(profit)
+#     if profit > max_profit and profit > 2:
+#         max_profit = profit
+#         total +=profit
+#         min_price = prices[i]
+#         max_profit = 0
+#         i+=1
+#         # print(max_profit)
+#         # min_price = max(prices)
+#     i+=1
+# print(total)
 
 
-s = "abcdefg"
-s = "abcd"
-s = list(s)
-res=''
-k = 2
-for i in range(k//2):
-    s[i],s[k-i-1] = s[k-i-1],s[i]
+# s = "abcdefg"
+# s = "abcd"
+# s = list(s)
+# res=''
+# k = 2
+# for i in range(k//2):
+#     s[i],s[k-i-1] = s[k-i-1],s[i]
 
-print((res.join(s)))
+# print((res.join(s)))
 
 # reverse string ii
 s = "abcdefg"
