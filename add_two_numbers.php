b5c244e0e3b059dfae0115331e0daf710018800f
shyqulreyad <shyqulislam@gmail.com>
2023-07-15 11:03:58 +0600

    july 15th

--- add_two_numbers.php
+++ add_two_numbers.php
@@ -7,15 +7,15 @@ function custom_reverse($arr) {
     }
     return $result;
 }
-$l1 = [9,9,9,9,9,9,9];
-$l2 = [9,9,9,9];
-$l1 = custom_reverse($l1);
-$l2 = array_reverse($l2);
-$num_one =implode($l1);
-$num_two =implode($l2);
-$result = $num_one + $num_two;
-$result = str_split($result);
-$result = array_reverse($result);
+// $l1 = [9,9,9,9,9,9,9];
+// $l2 = [9,9,9,9];
+// $l1 = custom_reverse($l1);
+// $l2 = array_reverse($l2);
+// $num_one =implode($l1);
+// $num_two =implode($l2);
+// $result = $num_one + $num_two;
+// $result = str_split($result);
+// $result = array_reverse($result);
 print_r($result);
 
 ?>
